ab46d3eefb98aa97108ff64541cafba5aac3f59d
laneatmore <52425233+laneatmore@users.noreply.github.com>
2024-01-19 11:04:42 +0100

    Create angsd_sfs_ind_het_est.sh
    Generate by ind sfs and estimate heterozygosity from saf

---
+++ demographic_modeling/SFS/angsd_sfs_ind_het_est.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --account=nn9244k
+#SBATCH --time=8:00:00
+#SBATCH --job-name=angsd_theta
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+
+set -o errexit
+
+module --quiet purge
+module load angsd/0.940-GCC-11.2.0
+
+prefix=$1
+REF=/cluster/projects/nn9244k/databases/herring/ref_genome/Ch_v2.0.2
+#nInd=153
+ANC=/cluster/projects/nn9244k/databases/herring/ref_genome/clupea_harengus_ancestor_Ch_v2.0.2/clupea_harengus_ancestor
+PATH_TO_BAMs=/cluster/work/users/lanea/angsd/data/bam_files/
+
+angsd -i $PATH_TO_BAMs/$prefix.Her_nu.bam -anc $ANC.fasta -dosaf 1 \
+-ref $REF.fasta -C 50 \
+-minMapQ 30 -minQ 20 -doMajorMinor 2 -doCounts 1 -noTrans 1 \
+-gl 1 -out $prefix.ancestor
+
+realSFS $prefix.ancestor.saf.idx > het_est/$prefix.ancestor.est.ml
+
+angsd -i $PATH_TO_BAMs/$prefix.Her_nu.bam -anc $REF.fasta -dosaf 1 \
+-ref $REF.fasta -C 50 \
+-minMapQ 20 -minQ 20 -rmTrans 1 \
+-doMajorMinor 2 -doCounts 1 -doMaf 1 -gl 1 -out $prefix.folded
+
+realSFS $prefix.folded.saf.idx -fold 1 > het_est/$prefix.folded.est.ml
